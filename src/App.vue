24bd24ef5285b1e363e7b5bb3c27fa0b57379c8e
WoCaoiOS <550769502@qq.com>
2017-10-11 10:53:38 +0800

    Signed-off-by: WoCaoiOS <550769502@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
     <router-link to="/my">跳转</router-link>
     <router-view></router-view>
+    <el-button type="primary" @click="getData">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
+    <ul>
+      <li v-for="obj of bookList">{{obj.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+// 导入qs模块
+import qs from 'qs'
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      bookList:[]
+    }
+  },
+  methods:{
+    getData(){
+      this.$http({
+        method:"post",
+        url:"http://route.showapi.com/92-92",
+        // 当与后台交互，要求使用application/x-www-form-urlencoded格式时，可以用qs模块进行转换
+        data:qs.stringify({
+          showapi_appid:44993,
+          showapi_sign:'82829efed7b04e2a8df1dd79a1814c55',
+          id:6
+        })
+      })
+      // 此处使用箭头函数，以便将请求得到的结果赋值给组件中的数据
+      .then(response => {
+        console.log(response);
+        this.bookList = response.data.showapi_res_body.bookList;
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import VueRouter from 'vue-router'  //引入路由模块
 import routes from './router.js' //导入路由配置文件
 import ElementUI from 'element-ui'  //导入element-ui
 import 'element-ui/lib/theme-default/index.css' //导入element-ui的css文件
+import axios from 'axios'
 import App from './App.vue'
 
 Vue.use(VueRouter);
 Vue.use(ElementUI);
+
+// 修改原型链
+Vue.prototype.$http = axios;
 
 // 创建路由实例
 const router = new VueRouter({
