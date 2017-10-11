d74f40b72d24e040e596bdbcde11805e1f7232fc
WoCaoiOS <550769502@qq.com>
2017-10-11 10:06:59 +0800

    Signed-off-by: WoCaoiOS <550769502@qq.com>

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'  //引入路由模块
 import routes from './router.js' //导入路由配置文件
+import ElementUI from 'element-ui'  //导入element-ui
+import 'element-ui/lib/theme-default/index.css' //导入element-ui的css文件
 import App from './App.vue'
 
 Vue.use(VueRouter);
+Vue.use(ElementUI);
 
 // 创建路由实例
 const router = new VueRouter({
